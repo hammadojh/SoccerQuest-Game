13af1fe630a7611933160f552f1fd3cca38c57d8
omar hammad <hammado92@gmail.com>
2016-01-30 13:15:30 +0300

    First Version - field and ball
    has these :
    - field
    - ball
    - script to move ball

---
+++ Assets/Scripts/moveBall.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+// the rigid body of the ball
+var rb : Rigidbody;
+var speed : int = 3.0;
+
+// runs first
+function Start () {
+
+	rb = GetComponent.<Rigidbody>();
+
+}
+
+// called once per frame
+function Update () {
+
+}
+
+// called before performing any fisics calculations
+function FixedUpdate () {
+
+	var moveH = Input.GetAxis("Horizontal");
+	var moveV = Input.GetAxis("Vertical");
+	
+	var movement = new Vector3(moveH*speed,0,moveV*speed);
+	
+	rb.AddForce(movement);
+
+}
